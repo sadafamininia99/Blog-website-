19357c249f6aa2a19ad28816ce6a3039496da9fe
SadafAmininiiiaa <aminicomputer96@gmail.com>
2022-12-02 21:29:46 +0330

    blog page

---
+++ src/Components/Blogs.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUserInput, setBlogData } from "../features/userSlice";
+
+import "../styling/blogs.css";
