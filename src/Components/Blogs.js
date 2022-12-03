6da92dfb8c38477a7422d037a16f052f2bd446f6
SadafAmininiiiaa <aminicomputer96@gmail.com>
2022-12-03 21:46:20 +0330

    blog page (useeffect)

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -4,3 +4,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectUserInput, setBlogData } from "../features/userSlice";
 
 import "../styling/blogs.css";
+
+const Blogs = () => {
+  const searchInput = useSelector(selectUserInput);
+  const blog_url = `https://gnews.io/api/v4/search?q=${searchInput}&token=74da443369bd202a1f3146a275d761fc`;
+  const dispatch = useDispatch();
+  const [blogs, setBlogs] = useState();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(blog_url)
+      .then((response) => {
+        dispatch(setBlogData(response.data));
+        setBlogs(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [searchInput]);
+}
