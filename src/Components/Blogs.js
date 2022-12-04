6723462cbfd854beef3e40e4ce1919d3d8479d73
SadafAmininiiiaa <aminicomputer96@gmail.com>
2022-12-04 13:19:04 +0330

    blog page now complete

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -26,3 +26,34 @@ const Blogs = () => {
       });
   }, [searchInput]);
 }
+
+  return (
+    <div className="blog__page">
+      <h1 className="blog__page__header">Blogs</h1>
+      {loading ? <h1 className="loading">Loading...</h1> : ""}
+      <div className="blogs">
+        {blogs?.articles?.map((blog) => (
+          <a className="blog" target="_blank" href={blog.url}>
+            <img src={blog.image} />
+            <div> 
+              <h3 className="sourceName">
+                <span>{blog.source.name}</span>
+                <p>{blog.publishedAt}</p>
+              </h3>
+              <h1>{blog.title}</h1>
+              <p>{blog.description}</p>
+            </div>
+          </a>
+        ))}
+
+        {blogs?.totalArticles == 0 && (
+          <h1 className="no__blogs">
+            No blogs available 😞. Search something else to read blogs on the
+            greatest platform.
+          </h1>
+        )}
+      </div>
+    </div>
+  );
+
+export default Blogs;
