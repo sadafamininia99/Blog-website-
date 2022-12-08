897e501e8742f476381c94beb1298d5046c2c866
Sadaf Amini-nia <66588239+sadafamininia99@users.noreply.github.com>
2022-12-09 00:00:37 +0330

    Create Navbar.js
    update

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import { Avatar } from "@material-ui/core";
+import React, { useState } from "react";
+import { GoogleLogout } from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  selectUserData,
+  setInput,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
