85a9b896cac85d0c266c39bff5f2b1d6974a34eb
SadafAmininiiiaa <aminicomputer96@gmail.com>
2022-12-08 23:12:49 +0330

    home page

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -6,3 +6,51 @@ import {
   setSignedIn,
   setUserData,
 } from "../features/userSlice";
+
+import "../styling/home.css";
+
+const Homepage = () => {
+  const isSignedIn = useSelector(selectSignedIn);
+
+  const dispatch = useDispatch();
+  const login = (response) => {
+    console.log(response);
+    dispatch(setSignedIn(true));
+    dispatch(setUserData(response.profileObj));
+  };
+
+  return (
+    <div className="home__page" style={{ display: isSignedIn ? "none" : "" }}>
+      {!isSignedIn ? (
+        <div className="login__message">
+          <h2>📗</h2>
+          <h1>A Readers favourite place!</h1>
+          <p>
+            We provide high quality online resource for reading blogs. Just sign
+            up and start reading some quality blogs.
+          </p>
+          <GoogleLogin
+            clientId="1084334404054-jch76dh3eqbj787ostatku2t93h61muv.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="login__button"
+              >
+                Login with Google
+              </button>
+            )}
+            onSuccess={login}
+            onFailure={login}
+            isSignedIn={true }
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Homepage;
