b1f6159d27787da91552a6f552cc9afcf95d9aad
Sadaf Amini-nia <66588239+sadafamininia99@users.noreply.github.com>
2022-12-09 22:22:34 +0330

    Update userSlice.js

---
+++ src/features/userSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    isSignedIn: false,
+    userData: null,
+    searchInput: "tech",
+    blogData: null,
