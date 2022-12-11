ec046916f4c93aad8303a36579cd3c826a907581
Sadaf Amini-nia <66588239+sadafamininia99@users.noreply.github.com>
2022-12-12 00:12:22 +0330

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,3 +9,70 @@ import {
   setSignedIn,
   setUserData,
 } from "../features/userSlice";
+
+import "../styling/navbar.css";
+
+const Navbar = () => {
+  const [inputValue, setInputValue] = useState("tech");
+  const isSignedIn = useSelector(selectSignedIn);
+  const userData = useSelector(selectUserData);
+
+  const dispatch = useDispatch();
+
+  const logout = (response) => {
+    dispatch(setSignedIn(false));
+    dispatch(setUserData(null));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(setInput(inputValue));
+  };
+
+  return (
+    <div className="navbar">
+      <h1 className="navbar__header">BlogMania 💬</h1>
+      {isSignedIn && (
+        <div className="blog__search">
+          <input
+            className="search"
+            placeholder="Search for a blog"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="submit" onClick={handleClick}>
+            Search
+          </button>
+        </div>
+      )}
+
+      {isSignedIn ? (
+        <div className="navbar__user__data">
+          <Avatar
+            className="user"
+            src={userData?.imageUrl}
+            alt={userData?.name}
+          />
+          <h1 className="signedIn">{userData?.givenName}</h1>
+          <GoogleLogout
+            clientId="1084334404054-jch76dh3eqbj787ostatku2t93h61muv.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="logout__button"
+              >
+                Logout 😦
+              </button>
+            )}
+            onLogoutSuccess={logout}
+          />
+        </div>
+      ) : (
+        <h1 className="notSignedIn">User not available 😞</h1>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
